9f2df745b7fe8f47084a283f89c634cec9dadd71
adex234 <adex@git.com>
2025-05-27 10:46:19 +0200

    Update

--- app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/tasks/MinecraftDownloader.java
+++ app_pojavlauncher/src/main/java/net/kdt/pojavlaunch/tasks/MinecraftDownloader.java
@@ -391,4 +391,153 @@ public class MinecraftDownloader {
     private void scheduleLoggingAssetDownloadIfNeeded(JMinecraftVersionList.LoggingConfig loggingConfig) throws IOException {
         if(loggingConfig.client == null || loggingConfig.client.file == null) return;
         JMinecraftVersionList.FileProperties loggingFileProperties = loggingConfig.client.file;
-        File internalLoggingConfig = new File(Tools.DI
+        File internalLoggingConfig = new File(Tools.DIR_DATA + File.separator + "security",
+                loggingFileProperties.id.replace("client", "log4j-rce-patch"));
+        if(internalLoggingConfig.exists()) return;
+        File destination = new File(Tools.DIR_GAME_NEW, loggingFileProperties.id);
+        scheduleDownload(destination,
+                DownloadMirror.DOWNLOAD_CLASS_LIBRARIES,
+                loggingFileProperties.url,
+                loggingFileProperties.sha1,
+                loggingFileProperties.size,
+                false);
+    }
+
+    private void scheduleGameJarDownload(MinecraftClientInfo minecraftClientInfo, String versionName) throws IOException {
+        File clientJar = createGameJarPath(versionName);
+        String clientSha1 = LauncherPreferences.PREF_CHECK_LIBRARY_SHA ?
+                minecraftClientInfo.sha1 : null;
+        growDownloadList(1);
+        scheduleDownload(clientJar,
+                DownloadMirror.DOWNLOAD_CLASS_LIBRARIES,
+                minecraftClientInfo.url,
+                clientSha1,
+                minecraftClientInfo.size,
+                false
+        );
+        // Store the path of the JAR to copy it into our new version folder later.
+        mSourceJarFile = clientJar;
+    }
+
+    private static byte[] getLocalBuffer() {
+        byte[] tlb = sThreadLocalDownloadBuffer.get();
+        if(tlb != null) return tlb;
+        tlb = new byte[32768];
+        sThreadLocalDownloadBuffer.set(tlb);
+        return tlb;
+    }
+
+    private final class DownloaderTask implements Runnable, Tools.DownloaderFeedback {
+        private final File mTargetPath;
+        private final String mTargetUrl;
+        private String mTargetSha1;
+        private final int mDownloadClass;
+        private final boolean mSkipIfFailed;
+        private int mLastCurr;
+        private final long mDownloadSize;
+
+        DownloaderTask(File targetPath, int downloadClass, String targetUrl, String targetSha1,
+                       long downloadSize, boolean skipIfFailed) {
+            this.mTargetPath = targetPath;
+            this.mTargetUrl = targetUrl;
+            this.mTargetSha1 = targetSha1;
+            this.mDownloadClass = downloadClass;
+            this.mDownloadSize = downloadSize;
+            this.mSkipIfFailed = skipIfFailed;
+        }
+
+        private String downloadSha1() throws IOException {
+            String downloadedHash = DownloadMirror.downloadStringMirrored(
+                    mDownloadClass, mTargetUrl + ".sha1"
+            );
+            if(!Tools.isValidString(downloadedHash)) return null;
+            // Ensure that we don't have leading/trailing whitespaces before checking hash length
+            downloadedHash = downloadedHash.trim();
+            // SHA1 is made up of 20 bytes, which means 40 hexadecimal digits, which means 40 chars
+            if(downloadedHash.length() != 40) return null;
+            return downloadedHash;
+        }
+
+        /*
+         * Maven repositories usually have the hash of a library near it, like:
+         * .../libraryName-1.0.jar
+         * .../libraryName.1.0.jar.sha1
+         * Since Minecraft libraries are stored in maven repositories, try to use
+         * this when downloading libraries without hashes in the json.
+         */
+        private void tryGetLibrarySha1() {
+            String resultHash = null;
+            try {
+                resultHash = downloadSha1();
+                // The hash is a 40-byte download.
+                mInternetUsageCounter.getAndAdd(40);
+            }catch (IOException e) {
+                Log.i("MinecraftDownloader", "Failed to download hash", e);
+            }
+            if(resultHash != null) {
+                Log.i("MinecraftDownloader", "Got hash: "+resultHash+ " for "+FileUtils.getFileName(mTargetUrl));
+                mTargetSha1 = resultHash;
+            }
+        }
+
+        @Override
+        public void run() {
+            try {
+                runCatching();
+            }catch (Exception e) {
+                mDownloaderThreadException.set(e);
+            }
+        }
+
+        private void runCatching() throws Exception {
+            if(mDownloadClass == DownloadMirror.DOWNLOAD_CLASS_LIBRARIES && !Tools.isValidString(mTargetSha1)) {
+                // If we're downloading a library, try to get sha1 since it might be available as a file
+                tryGetLibrarySha1();
+            }
+            if(Tools.isValidString(mTargetSha1)) {
+                verifyFileSha1();
+            }else {
+                mTargetSha1 = null; // Nullify SHA1 as DownloadUtils.ensureSha1 only checks for null,
+                                    // not for string validity
+                if(mTargetPath.exists()) finishWithoutDownloading();
+                else downloadFile();
+            }
+        }
+        
+        private void verifyFileSha1() throws Exception {
+            if(mTargetPath.isFile() && mTargetPath.canRead() && Tools.compareSHA1(mTargetPath, mTargetSha1)) {
+                finishWithoutDownloading();
+            } else {
+                // Rely on the download function to throw an IOE in case if the file is not
+                // writable/not a file/etc...
+                downloadFile();
+            }
+        }
+        
+        private void downloadFile() throws Exception {
+            try {
+                DownloadUtils.ensureSha1(mTargetPath, mTargetSha1, () -> {
+                    DownloadMirror.downloadFileMirrored(mDownloadClass, mTargetUrl, mTargetPath,
+                            getLocalBuffer(), this);
+                    return null;
+                });
+            }catch (Exception e) {
+                if(!mSkipIfFailed) throw e;
+            }
+            mProcessedFileCounter.incrementAndGet();
+        }
+
+        private void finishWithoutDownloading() {
+            mProcessedFileCounter.incrementAndGet();
+            mProcessedSizeCounter.addAndGet(mDownloadSize);
+        }
+
+        @Override
+        public void updateProgress(int curr, int max) {
+            int delta = curr - mLastCurr;
+            mProcessedSizeCounter.addAndGet(delta);
+            mInternetUsageCounter.addAndGet(delta);
+            mLastCurr = curr;
+        }
+    }
+}
